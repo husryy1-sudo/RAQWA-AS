5148a4bcecd7ae0f5ac98ac2d599b36b28e31d04
husryy1-sudo <husryy1@gmail.com>
2026-01-17 01:31:09 +0200

    Add files via upload

---
+++ src/components/src/components/admin/AdminLayout.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Sidebar } from './Sidebar';
+import { Header } from './Header';
+
+interface AdminLayoutProps {
+    children: React.ReactNode;
+    title: string;
+}
+
+export const AdminLayout: React.FC<AdminLayoutProps> = ({ children, title }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+
+            <div className="lg:ml-64 min-h-screen flex flex-col">
+                <Header onMenuClick={() => setIsSidebarOpen(true)} title={title} />
+
+                <main className="flex-1 p-4 sm:p-6 lg:p-8">
+                    <div className="max-w-7xl mx-auto w-full">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+};
