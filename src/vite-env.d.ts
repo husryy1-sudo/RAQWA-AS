0a7ce459655f28ba7de134d7f3985e6be61853aa
husryy1-sudo <husryy1@gmail.com>
2026-01-16 23:43:47 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+declare module 'qr-code-styling' {
+    export type Options = any;
+    export type DrawType = 'canvas' | 'svg';
+    export type TypeNumber = number;
+    export type Mode = 'Canvas' | 'SVG' | 'Byte' | 'Numeric' | 'Alphanumeric' | 'Kanji';
+    export type ErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H';
+    export type DotType = 'dots' | 'rounded' | 'classy' | 'classy-rounded' | 'square' | 'extra-rounded';
+    export type CornerSquareType = 'dot' | 'square' | 'extra-rounded';
+    export type CornerDotType = 'dot' | 'square';
+
+    export default class QRCodeStyling {
+        constructor(options?: any);
+        append(container?: HTMLElement): void;
+        update(options?: any): void;
+        download(downloadOptions?: { name?: string; extension?: string }): Promise<void>;
+        getRawData(extension: string): Promise<Blob | null>;
+    }
+}
+
+declare module 'react-colorful';
